f31e9ff5c3f413a65ff08a20edd799963459301f
willianctti <willianctti@gmail.com>
2023-01-05 11:07:49 -0300

    .

---
+++ aula014vetoresex/script.js
@@ -0,0 +1,6 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function adicionar() {}
