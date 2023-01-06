2523553a789b933745bd6fd83f9b54a45c46548d
willianctti <willianctti@gmail.com>
2023-01-06 11:25:45 -0300

    var e let em loop, resultados e hoisting

---
+++ usandoVAR/1.js
@@ -0,0 +1,17 @@
+{
+    {
+        {
+            { 
+                var sera = 'Sera???'
+            }
+        }
+    }
+}
+console.log(sera)
+
+function teste() {
+    var local = 123
+
+}
+
+console.log(local)
