56c253e92c93af061d83d1dfcfa50adc5f5d3b51
willianctti <willianctti@gmail.com>
2023-01-10 10:21:47 -0300

    .

--- funcao/thisEBind2.js
+++ funcao/thisEBind2.js
@@ -1,10 +1,12 @@
 function pessoa() {
     this.idade = 0
 
+
+    const self = this
     setInterval(function () {
-        this.idade++
-        console.log(this.idade)
-    }.bind(this), 1000)
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)
 }
 
 new pessoa
