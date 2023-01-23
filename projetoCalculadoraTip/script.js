9836cbeaffb536f4edc42df5f7058dc1c257c9a2
willianctti <willianctti@gmail.com>
2023-01-23 20:06:42 -0300

    .

---
+++ projetoCalculadoraTip/script.js
@@ -0,0 +1,6 @@
+function update() {
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+
+}
